77b18a6a75241f2480a259be220d402c8fe83346
mlemke24 <mattrlemke@gmail.com>
2021-05-10 17:34:08 -0500

    login done

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -52,25 +52,32 @@ router.get('/:id', (req,res) => {
 router.post('/', (req, res) => {
     User.create({
         username: req.body.username,
-        email: req.body.email,
         password: req.body.password
       })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
+      .then(dbUserData => {
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+    
+          res.json(dbUserData);
         });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
     });
 
 // Login
 router.post('/login', (req, res) =>{
   User.findOne({
     where: {
-      email: req.body.email
+      username: req.body.username
     }
   }).then(dbUserData => {
     if (!dbUserData) {
-      res.status(400).json({ message: 'No user with that email address!' });
+      res.status(400).json({ message: 'No user with that username!' });
       return;
     }
 
@@ -79,10 +86,15 @@ router.post('/login', (req, res) =>{
       res.status(400).json({ message: 'Incorrect password!' });
       return;
     }
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
 
     res.json({ user: dbUserData, message: 'You are now logged in!' });
   });
-})
+ })
+});
 
 // PUT api/users/id
 router.put('/:id', (req, res) => {
