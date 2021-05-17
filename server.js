974e5af9902f6c357c360d2fcaa3b804da98bba2
mlemke24 <mattrlemke@gmail.com>
2021-05-17 09:43:22 -0500

    heroku refactor

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const path = require('path');
-const helpers = require('./utils/helpers');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
+const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -23,20 +21,19 @@ const sess = {
 
 app.use(session(sess));
 
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-// stylesheet and JS
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
- 
-// turn on routes
-app.use(require('./controllers'));
 
-// turn on connection to db and server
+app.use(require('./controllers/'));
+
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-  });
-
+  app.listen(PORT, () => console.log('Now listening'));
+});
